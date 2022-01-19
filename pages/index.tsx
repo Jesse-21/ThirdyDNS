244c4484c8ef307bb9adbdf772b2f43b52dd2d7a
yektas <ahmetsercanyektas@gmail.com>
2022-01-19 03:45:53 +0300

    start building ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,31 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="mx-auto">
-        <h1 className="">Welcome to NextJS and Hardhat template</h1>
+    <>
+      <div className="fixed">
+        <img src="/bg3.svg" className="w-[500px] h-[500px]" />
       </div>
-    </div>
+      <div className="flex flex-col items-center justify-start h-screen pt-64 space-y-10 text-white bg-black">
+        {/*  <img src="/bg2.svg" className="absolute w-full h-full " /> */}
+
+        <h1 className="text-4xl">Your next awesome domain is here. Claim it!</h1>
+        <div className="w-1/3 mb-40">
+          <div className="relative w-full shadow-sm ">
+            <input
+              type="text"
+              name="name"
+              className="block w-full pr-24 text-black border-none h-14 focus:ring-primary sm:text-sm"
+              placeholder="Search your next ENS name"
+            />
+            <div className="absolute inset-y-0 right-0 flex items-center h-14">
+              <div className=" absolute inset-y-0 right-0 flex items-center px-4 ml-3 font-semibold  pointer-events-none bg-[#ab1251]">
+                SEARCH
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
